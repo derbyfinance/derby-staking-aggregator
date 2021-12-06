4368ae256da45d37d21ce7a13b86a090d99e2e3d
sjoerdsommen <sjoerdsommen@gmail.com>
2021-12-06 16:58:30 +0100

    added some packages

---
+++ test/helpers/deploy.ts
@@ -0,0 +1,8 @@
+// import { deployContract } from 'ethereum-waffle';
+// import { Signer, BigNumberish } from 'ethers';
+
+// import BasketToken from '../../artifacts/contracts/BasketToken.sol/BasketToken.json';
+// import { BasketToken } from '@typechain/BasketToken';
+
+// export const deployBasketToken = (deployerSign: Signer, ETFgame: Address, name: String, symbol: String): Promise<BasketToken> => {
+//     return (deployContract(deployerSign, BasketTokenArtifact, [ETFgame, name, symbol])) as Promise<BasketToken>;
