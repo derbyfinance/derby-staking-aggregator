327fcb8066b9c4282e83b363c1a9a67f4cb7e15d
Theezr <nickvanrossenberg@hotmail.com>
2022-02-07 13:45:43 +0100

    linting router

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -28,26 +28,42 @@ contract Router {
     _;
   }
 
+  /// @notice Deposit the underlying asset in given protocol number
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @param _vault Address from ETFVault contract i.e buyer
+  /// @param _amount Amount to deposit
+  /// @return Deposit function for requested protocol
   function deposit(
     uint256 _ETFnumber, 
     uint256 _protocolNumber, 
-    address _buyer, 
+    address _vault, 
     uint256 _amount
     ) 
     external onlyVault returns(uint256) {
-      return IProvider(protocol[_ETFnumber][_protocolNumber]).deposit(_buyer, _amount);
+      return IProvider(protocol[_ETFnumber][_protocolNumber]).deposit(_vault, _amount);
   }
 
+  /// @notice Withdraw the underlying asset in given protocol number
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @param _vault Address from ETFVault contract i.e buyer
+  /// @param _amount Amount to withdraw
+  /// @return Withdraw function for requested protocol
   function withdraw(
     uint256 _ETFnumber, 
     uint256 _protocolNumber, 
-    address _seller, 
-    uint256 _balance
+    address _vault, 
+    uint256 _amount
     ) 
     external onlyVault returns(uint256) {
-      return IProvider(protocol[_ETFnumber][_protocolNumber]).withdraw(_seller, _balance);
+      return IProvider(protocol[_ETFnumber][_protocolNumber]).withdraw(_vault, _amount);
   }
 
+  /// @notice Exchange rate of underyling protocol token
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @return ExchangeRate function for requested protocol
   function exchangeRate(
     uint256 _ETFnumber, 
     uint256 _protocolNumber
@@ -56,6 +72,11 @@ contract Router {
       return IProvider(protocol[_ETFnumber][_protocolNumber]).exchangeRate();
   }
 
+  /// @notice Balance of  underlying Token from address
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @param _address Address to request balance from
+  /// @return Balance function for requested protocol
   function balance(
     uint256 _ETFnumber, 
     uint256 _protocolNumber,
@@ -65,6 +86,11 @@ contract Router {
       return IProvider(protocol[_ETFnumber][_protocolNumber]).balance(_address);
   }
 
+  /// @notice Get balance from address in shares i.e LP tokens
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @param _address Address to request balance from
+  /// @return balanceUnderlying function for requested protocol
   function balanceUnderlying(
     uint256 _ETFnumber, 
     uint256 _protocolNumber,
@@ -74,6 +100,11 @@ contract Router {
       return IProvider(protocol[_ETFnumber][_protocolNumber]).balanceUnderlying(_address);
   }
 
+  /// @notice Calculates how many shares are equal to the amount
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @param _amount Amount in underyling token e.g USDC
+  /// @return calcShares function for requested protocol
   function calcShares(
     uint256 _ETFnumber, 
     uint256 _protocolNumber,
@@ -83,6 +114,10 @@ contract Router {
       return IProvider(protocol[_ETFnumber][_protocolNumber]).calcShares(_amount);
   }
 
+  /// @notice Get underlying token address from protocol e.g USDC
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @return Token address function for requested protocol
   function getProtocolTokenAddress(
     uint256 _ETFnumber, 
     uint256 _protocolNumber
@@ -91,6 +126,11 @@ contract Router {
       return IProvider(protocol[_ETFnumber][_protocolNumber]).protocolToken();
   }
 
+  /// @notice Add protocol and vault to router
+  /// @param _ETFnumber Number of the ETFVault
+  /// @param _protocolNumber Protocol number linked to protocol vault
+  /// @param _provider Provider contract
+  /// @param _vault ETFVault address to whitelist
   function addProtocol(
     uint256 _ETFnumber, 
     uint256 _protocolNumber, 
