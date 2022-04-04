cab76c9b9a77ea3b11a4098a954425a1f308c3d5
mister_y <sjoerdsommen@gmail.com>
2022-04-04 11:47:19 +0200

    got rid of unAllocatedTokens

--- contracts/ETFGame.sol
+++ contracts/ETFGame.sol
@@ -61,9 +61,6 @@ contract ETFGame {
         // nr of total allocated tokens 
         uint256 nrOfAllocatedTokens;
 
-        // nr of locked tokens that are not allocated yet
-        uint256 nrOfUnAllocatedTokens;
-
         // total build up rewards
         uint256 totalUnRedeemedRewards;
 
@@ -89,15 +86,6 @@ contract ETFGame {
         require(IBasketToken(basketTokenAddress).ownerOf(_basketId) == msg.sender, "Not the owner of the Basket.");
 
         return baskets[_basketId].nrOfAllocatedTokens;
-    }
-
-    /// @notice function to see the total number of unallocated tokens. Only the owner of the basket can view this. 
-    /// @param _basketId Basket ID (tokenID) in the BasketToken (NFT) contract.
-    /// @return uint256 Number of xaver tokens that are locked in this contract but not (yet) allocated towards protocols.
-    function basketTotalUnAllocatedTokens(uint256 _basketId) public view returns(uint256){
-        require(IBasketToken(basketTokenAddress).ownerOf(_basketId) == msg.sender, "Not the owner of the Basket.");
-
-        return baskets[_basketId].nrOfUnAllocatedTokens;
     }
 
     /// @notice function to see the allocation of a specific protocol by a basketId. Only the owner of the basket can view this. 
@@ -132,7 +120,7 @@ contract ETFGame {
     /// @param _user User address from which the xaver tokens are locked inside this contract.
     /// @param _basketId Basket ID (tokenID) in the BasketToken (NFT) contract.
     /// @param _lockedTokenAmount Amount of xaver tokens to lock inside this contract.
-    function lockTokensToBasket(address _user, uint256 _basketId, uint256 _lockedTokenAmount) public {
+    function lockTokensToBasket(address _user, uint256 _basketId, uint256 _lockedTokenAmount) private {
         require(IBasketToken(basketTokenAddress).ownerOf(_basketId) == msg.sender, "Not the owner of the Basket.");
 
         uint256 balanceBefore = IERC20(xaverTokenAddress).balanceOf(address(this));
@@ -140,53 +128,45 @@ contract ETFGame {
         uint256 balanceAfter = IERC20(xaverTokenAddress).balanceOf(address(this));
         require((balanceAfter - balanceBefore - _lockedTokenAmount) == 0, "Error lock: under/overflow");
 
-        baskets[_basketId].nrOfUnAllocatedTokens += _lockedTokenAmount;
+        baskets[_basketId].nrOfAllocatedTokens += _lockedTokenAmount;
     }
 
     /// @notice Function to unlock xaver tokens. If tokens are still allocated to protocols they first hevae to be unallocated.  
     /// @param _user User address to which the xaver tokens are transferred from this contract.
     /// @param _basketId Basket ID (tokenID) in the BasketToken (NFT) contract.
     /// @param _lockedTokenAmount Amount of xaver tokens to lock inside this contract.
-    function unlockTokensFromBasket(address _user, uint256 _basketId, uint256 _lockedTokenAmount) public {
+    function unlockTokensFromBasket(address _user, uint256 _basketId, uint256 _lockedTokenAmount) private {
         require(IBasketToken(basketTokenAddress).ownerOf(_basketId) == msg.sender, "Not the owner of the Basket.");
-        require(baskets[_basketId].nrOfUnAllocatedTokens >= _lockedTokenAmount, "Not enough unallocated tokens in basket");
+        require(baskets[_basketId].nrOfAllocatedTokens >= _lockedTokenAmount, "Not enough unallocated tokens in basket");
 
         uint256 balanceBefore = IERC20(xaverTokenAddress).balanceOf(address(this));
         IERC20(xaverTokenAddress).safeTransfer(_user, _lockedTokenAmount);
         uint256 balanceAfter = IERC20(xaverTokenAddress).balanceOf(address(this));
         require((balanceBefore - balanceAfter - _lockedTokenAmount) == 0, "Error unlock: under/overflow");
 
-        baskets[_basketId].nrOfUnAllocatedTokens -= _lockedTokenAmount;
+        baskets[_basketId].nrOfAllocatedTokens -= _lockedTokenAmount;
     }
 
-    // // rebalances an existing Basket
-    // function rebalanceExistingBasket(uint256 _ETFnumber, uint256 _basketId, uint256[] memory _allocations) public {
-    //     require(IBasketToken(basketTokenAddress).ownerOf(_basketId) == msg.sender, "Not the owner of the Basket.");
+    // rebalances an existing Basket
+    function rebalanceBasket(uint256 _ETFnumber, uint256 _basketId, uint256[] memory _allocations) public {
+        require(IBasketToken(basketTokenAddress).ownerOf(_basketId) == msg.sender, "Not the owner of the Basket.");
 
-    //     addToTotalRewards(_basketId);
-    //     baskets[_basketId].latestAdjustmentPeriod = IETFVault(ETFVaults[_ETFnumber]).rebalancingPeriod() + 1;
+        addToTotalRewards(_basketId);
+        baskets[_basketId].latestAdjustmentPeriod = IETFVault(ETFVaults[_ETFnumber]).rebalancingPeriod() + 1;
         
-    //     uint256 totalNewAllocatedTokens = 0;
-    //     uint256 totalOldAllocatedTokens = baskets[_basketId].nrOfUnAllocatedTokens + baskets[_basketId].nrOfAllocatedTokens;
-    //     for (uint256 i = 0; i < _allocations.length; i++) {
-    //         totalNewAllocatedTokens += _allocations[i];
-    //         if (baskets[_basketId].allocations[i] == _allocations[i]) continue;
-    //         uint256 deltaAllocation = _allocations[i] - baskets[_basketId].allocations[i];
-    //         adjustDeltaAllocations(_ETFnumber, i, deltaAllocation);
-    //         baskets[_basketId].allocations[i] = _allocations[i];
-    //     }
+        uint256 totalNewAllocatedTokens = 0;
+        int256 deltaAllocation;
+        for (uint256 i = 0; i < _allocations.length; i++) {
+            totalNewAllocatedTokens += _allocations[i];
+            if (baskets[_basketId].allocations[i] == _allocations[i]) continue;
+            deltaAllocation = int256(_allocations[i] - baskets[_basketId].allocations[i]);
+            IETFVault(ETFVaults[_ETFnumber]).setDeltaAllocations(i, deltaAllocation);
+            baskets[_basketId].allocations[i] = _allocations[i];
+        }
 
-    //     if (totalNewAllocatedTokens > totalOldAllocatedTokens) {
-    //         uint256 lockedTokenAmount = totalNewAllocatedTokens - totalOldAllocatedTokens;
-    //         lockTokensToBasket(_basketId, lockedTokenAmount);
-    //         baskets[_basketId].nrOfUnAllocatedTokens = 0;
-            
-    //     } else {
-    //         if (totalNewAllocatedTokens <= baskets[_basketId].nrOfUnAllocatedTokens) baskets[_basketId].nrOfUnAllocatedTokens -= totalNewAllocatedTokens;
-    //         else baskets[_basketId].nrOfUnAllocatedTokens = 0;
-    //     }
-    //     baskets[_basketId].nrOfAllocatedTokens = totalNewAllocatedTokens;
-    // }
+        if (baskets[_basketId].nrOfAllocatedTokens > totalNewAllocatedTokens) unlockTokensFromBasket(msg.sender, _basketId, baskets[_basketId].nrOfAllocatedTokens - totalNewAllocatedTokens);
+        else if (baskets[_basketId].nrOfAllocatedTokens < totalNewAllocatedTokens) lockTokensToBasket(msg.sender, _basketId, totalNewAllocatedTokens - baskets[_basketId].nrOfAllocatedTokens);
+    }
 
     // // redeem funds from basket
     // function redeemRewards(uint256 _basketId, uint256 _amount) public {
@@ -203,18 +183,12 @@ contract ETFGame {
 
     // }
 
-    // // add to total rewards, formula of calculating the game rewards here
-    // function addToTotalRewards(uint256 _basketId) private {
-    //     baskets[_basketId].lastBasketPrice = 1;
-    //     uint256 amount = 0;
+    // add to total rewards, formula of calculating the game rewards here
+    function addToTotalRewards(uint256 _basketId) private {
+        baskets[_basketId].lastBasketPrice = 1;
+        uint256 amount = 0;
 
 
-    //     baskets[_basketId].totalUnRedeemedRewards += amount;
-    // }
-
-    // // adjusts the deltaAllocations in the ETF vault
-    // function adjustDeltaAllocations(uint256 _ETFnumber, uint256 _protocolId, uint256 _deltaAllocation) private {
-
-    // }
-
+        baskets[_basketId].totalUnRedeemedRewards += amount;
+    }
 }
--- contracts/Interfaces/IETFVault.sol
+++ contracts/Interfaces/IETFVault.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.11;
 interface IETFVault {
   function swapTokens(uint256 _amountIn, address _tokenIn) external returns(uint256);
   function rebalancingPeriod() external view returns(uint256);
+  function setDeltaAllocations(uint256 _protocolNum, int256 _allocation) external;
 }
