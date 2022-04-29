fc40b69f8c0b8eb45fe351e5e1c5cb991edf3d59
Theezr <nickvanrossenberg@hotmail.com>
2022-04-29 13:59:03 +0200

    idle addresses

--- test/helpers/addresses.ts
+++ test/helpers/addresses.ts
@@ -4,27 +4,33 @@ export const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 export const dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 export const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
-// Protocols
+/// Protocols
+// Yearn
 export const yearnUSDC = "0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE";
 export const compoundUSDC = "0x39AA39c021dfbaE8faC545936693aC917d5E7563";
 export const compoundDAI = "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643";
+// Aave
 export const aaveUSDC = "0xBcca60bB61934080951369a648Fb03DF4F96263C";
 export const aaveDAI = "0x028171bCA77440897B824Ca71D1c56caC55b68A3";
 export const aaveUSDT = "0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811";
+// TrueFi
 export const truefiUSDC = "0xA991356d261fbaF194463aF6DF8f0464F8f1c742";
 export const truefiUSDT = "0x6002b1dcB26E7B1AA797A17551C6F487923299d7";
-
 // Homora not really used
 export const homoraUSDC = "0x08bd64BFC832F1C2B3e07e634934453bA7Fa2db2";
 export const homoraUSDT = "0x020eDC614187F9937A1EfEeE007656C6356Fb13A";
 export const homoraDAI = "0xee8389d235E092b2945fE363e97CDBeD121A0439";
-
+// Idle
+export const idleUSDC = "0x5274891bEC421B39D23760c04A6755eCB444797C";
+export const idleUSDT = "0xF34842d05A1c888Ca02769A633DF37177415C2f8";
+export const idleDAI = "0x3fE7940616e5Bc47b0775a0dccf6237893353bB4";
 
 // Gov Tokens
 export const aave = "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9";
 export const yearn = "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e";
 export const truefi = "0x4C19596f5aAfF459fA38B0f7eD92F11AE6543784";
 export const alpha = "0xa1faa113cbE53436Df28FF0aEe54275c13B40975";
+export const idle = "0x875773784Af8135eA0ef43b5a374AaD105c5D39e";
 
 // Uniswap
 export const uniswapFactory = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
