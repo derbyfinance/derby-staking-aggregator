9d2818533394f3ed557e4ac9bcf05754bdbca3cf
mister_y <sjoerdsommen@gmail.com>
2022-08-03 12:37:01 +0200

    tests for connext transfer of funds

--- contracts/Mocks/XSendMock.sol
+++ contracts/Mocks/XSendMock.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.11;
 
 import "./Connext/interfaces/IXProviderMock.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract XSendMock {
+    using SafeERC20 for IERC20;
+
     address dao;
     address public xprovider; // ConnextXProviderMock
 
@@ -20,6 +24,12 @@ contract XSendMock {
 
     function xSendSomeValue(uint256 _value) external { // should later on be changed to onlyDao/ onlyKeeper
         IXProviderMock(xprovider).xSend(_value);
+    }
+
+    function xTransferFunds(address to, address asset, uint32 originDomain, uint32 destinationDomain, uint256 amount) external { // should later on be changed to onlyDao/ onlyKeeper
+        IERC20 token = IERC20(asset);       
+        token.approve(xprovider, amount);
+        IXProviderMock(xprovider).xTransfer(to, asset, originDomain, destinationDomain, amount);
     }
 
     function setXProvider(address _xprovider) external onlyDao {
--- test/unit/xChainMock.test.ts
+++ test/unit/xChainMock.test.ts
@@ -9,6 +9,9 @@ import { deployXSendMock, deployXReceiveMock, deployConnextXProviderMock, deploy
 import { usdc } from "../helpers/addresses";
 import { ethers } from "hardhat";
 
+const amount = 100_000;
+const amountUSDC = parseUSDC(amount.toString());
+
 describe.only("Testing XProviderMocks, unit test", async () => {
   let dao: Signer, user: Signer, USDCSigner: Signer, IUSDc: Contract, daoAddr: string, userAddr: string, xSend: XSendMock, xReceive: XReceiveMock, ConnextXProviderSend: ConnextXProviderMock, ConnextXProviderReceive: ConnextXProviderMock, ConnextExecutor: ConnextExecutorMock, ConnextHandler: ConnextHandlerMock, LZEndpointSend: LZEndpointMock, LZEndpointReceive: LZEndpointMock, LZXProviderSend: LZXProviderMock, LZXProviderReceive: LZXProviderMock;
 
@@ -35,6 +38,8 @@ describe.only("Testing XProviderMocks, unit test", async () => {
     await LZEndpointSend.setDestLzEndpoint(LZXProviderReceive.address, LZEndpointReceive.address);
     await LZXProviderSend.setTrustedRemote(2, LZXProviderReceive.address);
     await LZXProviderReceive.setTrustedRemote(1, LZXProviderSend.address);
+
+    await IUSDc.connect(USDCSigner).transfer(daoAddr, amountUSDC);
   });
 
   it("Should send integer from XSendMock to XReceiveMock using Connext", async function() {
@@ -68,5 +73,22 @@ describe.only("Testing XProviderMocks, unit test", async () => {
     await xSend.xSendSomeValue(sendValue);
     let receivedValue = await xReceive.value();
     expect(sendValue).to.be.equal(receivedValue);
+  });
+
+  it("Should transfer funds with LayerZero xProvider using a Connext transfer", async function() {
+    xSend = await deployXSendMock(dao, daoAddr);
+    await xSend.setXProvider(LZXProviderSend.address);
+    xReceive = await deployXReceiveMock(dao, daoAddr);
+    await xReceive.setXProvider(LZXProviderReceive.address);
+
+    await LZXProviderSend.setxReceiveMock(xReceive.address, 2);
+    await LZXProviderReceive.setxReceiveMock(xReceive.address, 2);
+
+    await IUSDc.connect(dao).transfer(xSend.address, amountUSDC);
+    await xSend.xTransferFunds(xReceive.address, usdc, 1, 2, amountUSDC);
+    const balanceXSend = await IUSDc.balanceOf(xSend.address);
+    const balanceXReceive = await IUSDc.balanceOf(xReceive.address);
+    expect(balanceXReceive).to.be.equal(amountUSDC);
+    expect(balanceXSend).to.be.equal('0');
   });
 });
