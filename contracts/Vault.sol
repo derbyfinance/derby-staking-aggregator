bd91d19488f676c1a7629e67824b3a55635cf290
DolanPump <nickvanrossenberg@hotmail.com>
2023-04-12 13:02:51 +0200

    fixed type casting in StorePriceAndRewards vault

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -230,7 +230,7 @@ contract Vault is ReentrancyGuard {
       return;
     }
 
-    int256 priceDiff = int256(currentPrice - lastPrices[_protocolId]);
+    int256 priceDiff = int256(currentPrice) - int256(lastPrices[_protocolId]);
     int256 nominator = (int256(_totalUnderlying * performanceFee) * priceDiff);
     int256 totalAllocatedTokensRounded = totalAllocatedTokens / 1E18;
     int256 denominator = totalAllocatedTokensRounded * int256(lastPrices[_protocolId]) * 100; // * 100 cause perfFee is in percentages
