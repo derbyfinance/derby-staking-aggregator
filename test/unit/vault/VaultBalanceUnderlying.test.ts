1e8e812a318527e2da05e97fb8b921150ae13ef0
DolanPump <nickvanrossenberg@hotmail.com>
2023-05-01 15:06:42 +0200

    comments

--- test/unit/vault/VaultBalanceUnderlying.test.ts
+++ test/unit/vault/VaultBalanceUnderlying.test.ts
@@ -14,7 +14,7 @@ const getRandomAllocation = () => Math.floor(Math.random() * 100_000) + 100_00;
 describe('Testing balanceUnderlying for every single protocol vault', async () => {
   const IUSDc: Contract = erc20(usdc);
 
-  it('Should calc balanceUnderlying for all known protocols correctly', async function () {
+  it('Should calc balanceUnderlying for all known USDC! protocols correctly', async function () {
     const { vault, user } = await setupVault();
     const liquidityPerc = 10;
 
@@ -38,10 +38,10 @@ describe('Testing balanceUnderlying for every single protocol vault', async () =
       const expectedBalance =
         (amount - liquidityVault) * (protocol.allocation / totalAllocatedTokens);
 
-      console.log(`---------------------------`);
-      console.log(protocol.name);
-      console.log({ balanceUnderlying });
-      console.log({ expectedBalance });
+      // console.log(`---------------------------`);
+      // console.log(protocol.name);
+      // console.log({ balanceUnderlying });
+      // console.log({ expectedBalance });
 
       // 1000 margin cause of trading stables
       expect(Number(balanceUnderlying)).to.be.closeTo(expectedBalance, 1400);
