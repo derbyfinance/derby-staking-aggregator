4faf72a2cb574c6a13fefe1eabca1066a434e617
DolanPump <nickvanrossenberg@hotmail.com>
2023-05-04 09:39:38 +0200

    idle vault

--- test/unit/vault/VaultDepositWithdraw.test.ts
+++ test/unit/vault/VaultDepositWithdraw.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { Contract, Signer } from 'ethers';
 import { erc20, formatUSDC, formatUnits, parseUSDC, parseUnits } from '@testhelp/helpers';
-import { starterProtocols as protocols } from '@testhelp/addresses';
+import { allProtocols as protocols } from '@testhelp/addresses';
 import { setupVault } from './setup';
 import { MainVaultMock } from '@typechain';
 
@@ -9,6 +9,9 @@ describe('Testing Vault, unit test', async () => {
   const compoundVault = protocols.get('compound_usdc_01')!;
   const aaveVault = protocols.get('aave_usdc_01')!;
   const yearnVault = protocols.get('yearn_usdc_01')!;
+  const truefiVault = protocols.get('truefi_usdc_01')!;
+  const idleVault = protocols.get('idle_usdc_01')!;
+  const betaVault = protocols.get('beta_usdc_01');
 
   const amount = 100_000;
 
@@ -16,6 +19,8 @@ describe('Testing Vault, unit test', async () => {
     { protocol: compoundVault, priceScale: 18 },
     { protocol: aaveVault, priceScale: 0 },
     { protocol: yearnVault, priceScale: 6 },
+    { protocol: truefiVault, priceScale: 6 },
+    // { protocol: idleVault, priceScale: 18 },
   ];
 
   tests.forEach(({ protocol, priceScale }) => {
@@ -44,7 +49,7 @@ describe('Testing Vault, unit test', async () => {
         const realBalance = await LPToken.balanceOf(vault.address);
         expect(formatUnits(expectedShares, decimals)).to.be.closeTo(
           formatUnits(realBalance, decimals),
-          0.1,
+          0.01,
         );
       });
 
@@ -53,10 +58,10 @@ describe('Testing Vault, unit test', async () => {
           await LPToken.balanceOf(vault.address),
         );
         const price = await vault.price(protocol.number);
-        // console.log({ expectedPrice });
-        // console.log({ price });
-        // console.log(formatUnits(expectedPrice, decimals));
-        // console.log(formatUnits(price, decimals));
+        console.log({ expectedPrice });
+        console.log({ price });
+        console.log(formatUnits(expectedPrice, decimals));
+        console.log(formatUnits(price, decimals));
         expect(formatUnits(expectedPrice, decimals)).to.be.closeTo(
           formatUnits(price, decimals),
           0.0001,
@@ -74,7 +79,7 @@ describe('Testing Vault, unit test', async () => {
         const realBalance = await LPToken.balanceOf(vault.address);
         expect(formatUnits(expectedShares, decimals)).to.be.closeTo(
           formatUnits(realBalance, decimals),
-          0.1,
+          0.01,
         );
       });
     });
