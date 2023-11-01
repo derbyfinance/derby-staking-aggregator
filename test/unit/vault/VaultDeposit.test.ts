63eddcfc5a0a5a04a02723e31bf22b89a8c9a74b
mister_y <sjoerdsommen@gmail.com>
2023-11-01 10:57:56 +0100

    fixed vaultDepositWithdrawal

--- test/unit/vault/VaultDeposit.test.ts
+++ test/unit/vault/VaultDeposit.test.ts
@@ -13,21 +13,18 @@ describe('Testing Vault Deposits, unit test', async () => {
       ({ vault, user, IDAI } = await setupVault());
     });
     it('Set and check deposit request', async function () {
-      const amountBN = BigNumber.from('100000000000000000000000');
-      await vault.connect(user).depositRequest(amountBN);
+      await vault.connect(user).depositRequest(parseEther(100_000));
       
-      expect(await vault.connect(user).getDepositRequest()).to.be.equal(amountBN);
-      expect(await IDAI.balanceOf(vault.address)).to.be.equal(amountBN);
+      expect(await vault.connect(user).getDepositRequest()).to.be.equal(parseEther(100_000));
+      expect(await IDAI.balanceOf(vault.address)).to.be.equal(parseEther(100_000));
     });
 
     it('Deposit a second time before rebalance', async function () {
-      const amountBN1 = BigNumber.from('50000000000000000000000');
-      const amountBN2 = BigNumber.from('150000000000000000000000');
-      await vault.connect(user).depositRequest(amountBN1);
+      await vault.connect(user).depositRequest(parseEther(50_000));
       
-      expect(await vault.connect(user).getDepositRequest()).to.be.equal(amountBN2);
-      expect(await vault.getTotalDepositRequestsTest()).to.be.equal(amountBN2);
-      expect(await IDAI.balanceOf(vault.address)).to.be.equal(amountBN2);
+      expect(await vault.connect(user).getDepositRequest()).to.be.equal(parseEther(150_000));
+      expect(await vault.getTotalDepositRequestsTest()).to.be.equal(parseEther(150_000));
+      expect(await IDAI.balanceOf(vault.address)).to.be.equal(parseEther(150_000));
     });
 
     it('Redeem deposit before next rebalance', async function () {
@@ -45,8 +42,7 @@ describe('Testing Vault Deposits, unit test', async () => {
     });
 
     it('Deposit and cancel request', async function () {
-      const amountBN = BigNumber.from('10000000000000000000000');
-      await vault.connect(user).depositRequest(amountBN);
+      await vault.connect(user).depositRequest(parseEther(10_000));
 
       await vault.connect(user).cancelDepositRequest();
 
@@ -54,41 +50,32 @@ describe('Testing Vault Deposits, unit test', async () => {
 
     it('Test training state', async function () {
       const { vault, user, guardian } = await setupVault();
-      const amountBN1 = BigNumber.from('10000000000000000000000');
-      const amountBN2 = BigNumber.from('5000000000000000000000');
-      const amountBN3 = BigNumber.from('6000000000000000000000');
-      const amountBN4 = BigNumber.from('3000000000000000000000');
+
       await vault.connect(guardian).setTraining(true);
       // set maxTrainingDeposit to 10k
-      await vault.connect(guardian).setTrainingDeposit(amountBN1);
+      await vault.connect(guardian).setTrainingDeposit(parseEther(10_000));
 
       // not whitelisted
-      await expect(vault.connect(user).depositRequest(amountBN2)).to.be.revertedWith('');
+      await expect(vault.connect(user).depositRequest(parseEther(5_000))).to.be.revertedWith('');
 
       await vault.connect(guardian).addToWhitelist(user.address);
 
-      await vault.connect(user).depositRequest(amountBN3);
+      await vault.connect(user).depositRequest(parseEther(6_000));
 
-      expect(await vault.connect(user).getDepositRequest()).to.be.equal(amountBN3);
+      expect(await vault.connect(user).getDepositRequest()).to.be.equal(parseEther(6_000));
 
       // min deposit of 100 not reached
       await expect(vault.connect(user).depositRequest(95 * 1e6)).to.be.revertedWith('Minimum deposit');
 
       // max deposit of 10k reached
-      await expect(vault.connect(user).depositRequest(amountBN3)).to.be.revertedWith('');
+      await expect(vault.connect(user).depositRequest(parseEther(6_000))).to.be.revertedWith('');
 
       // User will have a total of 9k LPs, so should be fine
-      await vault.connect(user).depositRequest(amountBN4);
+      await vault.connect(user).depositRequest(parseEther(3_000));
     });
   });
 
   describe('Testing vault direct deposits, unit test', async () => {
-    const amountBN1 = BigNumber.from('10000000000000000000000'); //10k
-    const amountBN2 = BigNumber.from('100000000000000000000000'); //100k
-    const amountBN3 = BigNumber.from('505000000000000000000000'); //505k
-    const amountBN4 = BigNumber.from('50500000000000000000'); //5.05
-    const precision = BigNumber.from('1000000000000000000'); //1e18
-    const amountBN5 = BigNumber.from('10000000000000000000'); //1e19
     before(async () => {
       ({ vault, user, IDAI } = await setupVault());
     });
@@ -104,7 +91,7 @@ describe('Testing Vault Deposits, unit test', async () => {
       await vault.rebalance();
 
 
-      await vault.connect(user).deposit(amountBN1);
+      await vault.connect(user).deposit(parseEther(10_000));
 
       //LP token has 6 decimals
       expect(await vault.balanceOf(user.address)).to.be.equal('10000000000');
@@ -115,15 +102,15 @@ describe('Testing Vault Deposits, unit test', async () => {
         totalUnderlying = totalUnderlying.add(balanceUnderlying);
       }
 
-      expect(totalUnderlying).to.be.closeTo(amountBN1, precision);
-      await vault.connect(user).withdraw(amountBN1);
+      expect(totalUnderlying).to.be.closeTo(parseEther(10_000), parseEther(1));
+      await vault.connect(user).withdraw(parseEther(10_000));
       totalUnderlying = BigNumber.from('0');
       for (const protocol of allDAIVaults.values()) {
         const balanceUnderlying = await protocol.balanceUnderlying(vault);
-        expect(balanceUnderlying).to.be.closeTo('0', precision);
+        expect(balanceUnderlying).to.be.closeTo('0', parseEther(1));
         totalUnderlying = totalUnderlying.add(balanceUnderlying);
       }
-      expect(totalUnderlying).to.be.closeTo('0', precision);
+      expect(totalUnderlying).to.be.closeTo('0', parseEther(1));
     });
 
     it('Test calculateExchangeRate function', async function () {
@@ -137,17 +124,17 @@ describe('Testing Vault Deposits, unit test', async () => {
       await vault.setDeltaAllocationsReceivedTEST(true);
       await vault.rebalance();
 
-      await vault.connect(user).deposit(amountBN1);
+      await vault.connect(user).deposit(parseEther(10_000));
 
       //LP token has 6 decimals
       expect(await vault.balanceOf(user.address)).to.be.equal('10000000000');
 
-      let exchangeRate = await vault.calculateExchangeRate(amountBN1);
-      expect(exchangeRate).to.be.equal(precision);
-      exchangeRate = await vault.calculateExchangeRate(amountBN2);
-      expect(exchangeRate).to.be.equal(amountBN5);
-      exchangeRate = await vault.calculateExchangeRate(amountBN3);
-      expect(exchangeRate).to.be.equal(amountBN4);
+      let exchangeRate = await vault.calculateExchangeRate(parseEther(10_000));
+      expect(exchangeRate).to.be.equal(parseEther(1));
+      exchangeRate = await vault.calculateExchangeRate(parseEther(100_000));
+      expect(exchangeRate).to.be.equal(parseEther(10));
+      exchangeRate = await vault.calculateExchangeRate(parseEther(505_000));
+      expect(exchangeRate).to.be.equal(parseEther(50.5));
     });
   });
 });
